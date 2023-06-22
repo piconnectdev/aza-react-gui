559de59e6947cf745a4b7284c3d83f968f6e81bc
William Swanson <swansontec@gmail.com>
2023-06-22 11:56:06 -0700

    integrate package

--- metro.config.js
+++ metro.config.js
@@ -5,25 +5,19 @@
  * @format
  */
 
-const { getDefaultConfig } = require('metro-config')
+const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig()
+  } = await getDefaultConfig();
   return {
     transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true
-        }
-      }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer')
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg']
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
     }
-  }
-})()
+  };
+})();
