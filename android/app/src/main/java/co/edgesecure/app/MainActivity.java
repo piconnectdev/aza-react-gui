585326513c7f0f4d112535bbe9feeec97a16ca0b
William Swanson <swansontec@gmail.com>
2023-06-22 11:38:57 -0700

    wip integrate upstream

--- android/app/src/main/java/co/edgesecure/app/MainActivity.java
+++ android/app/src/main/java/co/edgesecure/app/MainActivity.java
@@ -3,9 +3,13 @@ package co.edgesecure.app;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import com.reactnativecomponent.splashscreen.RCTSplashScreen;
 
 public class MainActivity extends ReactActivity {
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -15,6 +19,24 @@ public class MainActivity extends ReactActivity {
     return "edge";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
+  }
+
+  // Edge addition for the splash screen
   protected void onCreate(Bundle savedInstanceState) {
     RCTSplashScreen.openSplashScreen(this);
 
@@ -24,6 +46,7 @@ public class MainActivity extends ReactActivity {
     }
   }
 
+  // Edge addition
   @Override
   public void invokeDefaultOnBackPressed() {
     moveTaskToBack(true);
